f6a514411edbf99b171c093d0a1a4d7f64c36014
greatjack1 <yaakovrim@gmail.com>
2018-06-08 16:20:40 -0400

    Fully working commit

--- src/com/wyre/Main.java
+++ src/com/wyre/Main.java
@@ -3,8 +3,6 @@ package com.wyre;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -25,44 +23,45 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-        // get the current directory
-        String currdir = System.getProperty("user.dir");
-        File fle = new File(currdir);
-        for (File file : fle.listFiles()) {
-            //if not an xml file then don't process
-            if ((!file.isFile()) || !file.getAbsoluteFile().toString().contains(".xml")) {
-                System.out.println(file.getName() + "is not an xml file, skipping");
-                continue;
-            }
-            System.out.println("Processing " + file.getName());
-            try {
-
-            // 1- Build the doc from the XML file
+        try {
+            File file = new File(System.getProperty("user.dir"));
+            for (File fle : file.listFiles()) {
+                //skip the file if it is a jar
+                if ((!fle.getName().contains(".xml"))) {
+                    System.out.println("file is not an xml file, skipping.");
+                    continue;
+                }
+                System.out.println("Processing " + fle.getName());
+                // 1- Build the doc from the XML file
                 Document doc = DocumentBuilderFactory.newInstance()
-                        .newDocumentBuilder().parse(new InputSource(file.getAbsolutePath()));
-                //delete the old file
-                file.delete();
-             // 2- Locate the node(s) with xpath
-                XPath xpath = XPathFactory.newInstance().newXPath();
-                NodeList nodes = (NodeList) xpath.evaluate("//*[contains(@value, '!Here')]",
-                        doc, XPathConstants.NODESET);
+                        .newDocumentBuilder().parse(fle);
 
-             // 3- Make the change on the selected nodes
+// 2- Locate the node(s) with xpath
+                XPath xpath = XPathFactory.newInstance().newXPath();
+                NodeList nodes = (NodeList) xpath.evaluate("//WebView",
+                        doc, XPathConstants.NODESET);
+//ensure the node list has elements so that we are not processing for nothing
+                if (nodes.getLength() <= 0) {
+                    System.out.println("file does not have any webviews, skipping");
+                    continue;
+                }
+                System.out.println("file has webviews, replacing.......");
+// 3- Make the change on the selected nodes
                 for (int idx = 0; idx < nodes.getLength(); idx++) {
                     Node value = nodes.item(idx).getAttributes().getNamedItem("android:layout_width");
-                    String val = value.getNodeValue();
                     value.setNodeValue("1dp");
                     Node value2 = nodes.item(idx).getAttributes().getNamedItem("android:layout_height");
-                    String val2 = value2.getNodeValue();
                     value2.setNodeValue("1dp");
                 }
-                //write the new file
-                // 4- Save the result to a new XML doc
+//erase the old file
+                fle.delete();
+// 4- Save the result to a new XML doc
                 Transformer xformer = TransformerFactory.newInstance().newTransformer();
-                xformer.transform(new DOMSource(doc), new StreamResult(new File(file.getAbsoluteFile().toString())));
-            } catch (Exception ex) {
-                System.out.println("Error: " + ex.getMessage());
+                xformer.transform(new DOMSource(doc), new StreamResult(new File(fle.getAbsoluteFile().toString())));
             }
+        } catch (Exception ex){
+            System.out.println("Error: " + ex.getMessage());
+        }
         }
     }
-}
+
