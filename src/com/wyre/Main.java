e9daad02407835b40a47152e99e7257ae232a706
greatjack1 <yaakovrim@gmail.com>
2018-06-09 22:31:37 -0400

    improved program output

--- src/com/wyre/Main.java
+++ src/com/wyre/Main.java
@@ -18,11 +18,12 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * This class goes through all xml files in the working directry and resizes the webviews to 1dp to effectivly make them dissapear
+ * This class goes through all xml files in the working directry and resizes the webviews to 1dp to effectivly make them disappear
  */
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Web view killer has started, please be patient as we process all of the files in the current directory........");
         try {
             File file = new File(System.getProperty("user.dir"));
             for (File fle : file.listFiles()) {
@@ -31,41 +32,38 @@ public class Main {
                     System.out.println("file is not an xml file, skipping.");
                     continue;
                 }
-                System.out.println("Processing " + fle.getName());
-                // 1- Build the doc from the XML file
                 Document doc = DocumentBuilderFactory.newInstance()
                         .newDocumentBuilder().parse(fle);
-
-// 2- Locate the node(s) with xpath
                 XPath xpath = XPathFactory.newInstance().newXPath();
                 NodeList nodes = (NodeList) xpath.evaluate("//WebView",
                         doc, XPathConstants.NODESET);
-//ensure the node list has elements so that we are not processing for nothing
                 if (nodes.getLength() <= 0) {
-                    System.out.println("file does not have any webviews, skipping");
                     continue;
                 }
-                System.out.println("file has webviews, replacing.......");
-// 3- Make the change on the selected nodes
+                System.out.println( fle.getName() + " has webviews, trying to replace them.......");
                 for (int idx = 0; idx < nodes.getLength(); idx++) {
                     Node value = nodes.item(idx).getAttributes().getNamedItem("android:layout_width");
-                    if(value!=null) {
+                    if (value != null) {
                         value.setNodeValue("1dp");
+                    } else {
+                        System.out.println("Error hiding webviews, please do this file manually. File name is " + fle.getName());
                     }
                     Node value2 = nodes.item(idx).getAttributes().getNamedItem("android:layout_height");
-                    if(value2!=null) {
+                    if (value2 != null) {
                         value2.setNodeValue("1dp");
+                    } else {
+                        System.out.println("Error hiding webviews, please do this file manually. File name is " + fle.getName());
                     }
                 }
-//erase the old file
+                //erase the old file and save the new one
                 fle.delete();
-// 4- Save the result to a new XML doc
                 Transformer xformer = TransformerFactory.newInstance().newTransformer();
                 xformer.transform(new DOMSource(doc), new StreamResult(new File(fle.getAbsoluteFile().toString())));
             }
-        } catch (Exception ex){
+        } catch (Exception ex) {
             System.out.println("Error: " + ex.getMessage());
         }
-        }
+        System.out.println("Web view killer has finished");
     }
+}
 
